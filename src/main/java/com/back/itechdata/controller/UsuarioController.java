bf16b05bbc22e06f333e06ead68995bae02e483d
Jorge Cevallos <jorge.l.cevallos@unl.edu.ec>
2023-01-05 10:58:41 -0500

    actualizar las direcciones de home

--- src/main/java/com/back/itechdata/controller/UsuarioController.java
+++ src/main/java/com/back/itechdata/controller/UsuarioController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.back.itechdata.model.Orden;
 import com.back.itechdata.model.Usuario;
@@ -43,12 +44,21 @@ public class UsuarioController {
 	}
 	
 	@PostMapping("/save")
-	public String save(Usuario usuario) {
-		logger.info("Usuario registro: {}", usuario);
-		usuario.setTipo("USER");
-		usuario.setPassword( passEncode.encode(usuario.getPassword()));
-		usuarioService.save(usuario);		
-		return "redirect:/";
+	public String save(Usuario usuario, Model modelo ) {
+		Optional<Usuario> optionalUser=usuarioService.findByEmail(usuario.getEmail());
+		logger.info("Este es el email del nuevo usuario: {}", usuario.getEmail());
+		if(!optionalUser.isPresent()) {
+			logger.info("Se guarda el nuevo usuario");
+			usuario.setTipo("USER");//seteamos el tipo de usuario con el que se van a registrar
+			usuario.setPassword( passEncode.encode(usuario.getPassword()));
+			usuarioService.save(usuario);
+			return "redirect:/";
+		}
+		logger.info("No se guarda el nuevo usuario");
+		modelo.addAttribute("error", "Ya existe un usuario con el mismo email");
+		 logger.info("No se guarda el nuevo usuario");
+		
+		return "usuario/registro";
 	}
 	
 	@GetMapping("/login")
